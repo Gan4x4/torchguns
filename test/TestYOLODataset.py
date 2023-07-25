777db10df2ebe40c3b733f35836160fad55052ff
anton <gan4x4@gmail.com>
2023-07-25 12:40:00 +0300

    Add bbox drawing on image

--- test/TestYOLODataset.py
+++ test/TestYOLODataset.py
@@ -1,11 +1,23 @@
 import unittest
 from torchguns.YOLODataset import YOLODataset
+from torchvision.utils import draw_bounding_boxes
+from torchvision.transforms.functional import to_pil_image
+
 
 class TestYOLODataset(unittest.TestCase):
     def test_getitem(self):
         ds = YOLODataset("test/data/yolo_dataset")
         for im, bbox in ds:
             self.assertEqual(len(bbox), 6)  # add assertion here
+            pil = TestYOLODataset.draw(im, bbox)
+            pil.save("test/out/tmp.jpg")
+            break
+
+    @staticmethod
+    def draw(im, bbox):
+        demo_im = draw_bounding_boxes(im, bbox[:, 1:], width=2)
+        pil = to_pil_image(demo_im)
+        return pil
 
 
 if __name__ == '__main__':
