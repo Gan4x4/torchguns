136be0330a5970fcd01daf009236c8c858ab03ab
Gan4x4 <gan4x4@gmail.com>
2023-11-24 15:06:37 +0300

    Add tests for and desired frames

--- test/testHSESubset.py
+++ test/testHSESubset.py
@@ -1,17 +1,37 @@
 import unittest
-from src.HSESubset import HSESubset
+from torchguns.HSESubset import HSESubset
 from .utils import draw
 
+
 class HSESubsetTest(unittest.TestCase):
+    path_download = "test/out/"
+    path_to_sample = "test/data/HSE/store_07"
 
-    @unittest.skip("")
+
     def test_base(self):
-        ds = HSESubset("test/data/HSEDataset/store_07")
-        for i, (img, bbox) in enumerate(ds):
+        ds = HSESubset(self.path_to_sample)
+        img, bbox = ds[0]
+        self.assertIsNotNone(img)
+        # pil = draw(img, bbox)
+        # pil.save(f"test/out/tmp_{ds.name}_1.jpg")
 
-            pil = draw(img, bbox)
-            pil.save(f"test/out/tmp{i}.jpg")
 
+    def test_desired_frames(self):
+        ds = HSESubset(self.path_to_sample, desired_frames=3)
+        self.assertEqual(3, len(ds))
+
+
+    def test_fps(self):
+        ds = HSESubset(self.path_to_sample, fps = 0.15)
+        self.assertEqual(8, len(ds))
+
+    def test_fps_and_desired_frames_together(self):
+        ds = HSESubset(self.path_to_sample, fps=0.15, desired_frames = 100)
+        self.assertWarns(Warning)
+        self.assertEqual(8, len(ds))
+
+
+"""
     def test_persons(self):
         ds = HSESubset("/home/anton/Code/MIEM/Weapon/gunrec/data/miem_test/office_03",build_cache=False)
         ds.desired_frames = 20
@@ -19,13 +39,7 @@ class HSESubsetTest(unittest.TestCase):
         for i, (img, bbox) in enumerate(ds):
             pil = draw(img, bbox)
             pil.save(f"test/out/tmp{i}.jpg")
-
+"""
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
-
-
