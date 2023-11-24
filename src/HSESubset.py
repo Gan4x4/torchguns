19e89a44733b7c6b66dc6fa695b37625c5d8c47c
Gan4x4 <gan4x4@gmail.com>
2023-11-24 13:13:35 +0300

    Add full support of fps and desired_frames properties

--- src/HSESubset.py
+++ src/HSESubset.py
@@ -9,15 +9,19 @@ class HSESubset(VideoDataset):
     class_filter = ['Person', 'Gun']
 
     def boxes(self, n, image=None):
+        # Read weapon boxes
         gun_boxes = super().boxes(n, image)
         if len(gun_boxes) > 0:
             gun_boxes[:, 0] = self.classes.index("Gun")  # Gun class
         filename = self.getfilename(n)
+
+        # Try to read persons boxes
         new_path = filename.replace('obj_train_data', 'persons')
         if not os.path.exists(new_path):
             return gun_boxes  # no persons
         h, w = YOLODataset.extract_hw(image)
         person_boxes = YOLODataset.read_yolo_file(new_path, h, w)
+        # merge person and weapon boxes
         all_boxes = torch.cat((gun_boxes, torch.Tensor(person_boxes)), dim=0)
 
         return all_boxes
--- src/VideoDataset.py
+++ src/VideoDataset.py
@@ -6,6 +6,9 @@ import cv2
 from PIL import Image
 from glob import glob
 from tqdm import tqdm
+import warnings
+
+
 class VideoDataset(YOLODataset):
     """ Wrapper of single videofile dataset """
 
@@ -19,22 +22,21 @@ class VideoDataset(YOLODataset):
             transforms: Optional[Callable] = None,
             transform: Optional[Callable] = None,
             target_transform: Optional[Callable] = None,
-            build_cache = True
+            build_cache=True
     ) -> None:
         self.video_folder = video_folder
-        self.settings = {'desired_frames': None}
+        # self.settings = {'desired_frames': None}
         self.load_settings(video_folder + os.sep + "settings.json")
         self.labels_dir = video_folder + "/obj_train_data/"
         cap = self.open_video()
-        self.fps = int(cap.get(cv2.CAP_PROP_FPS))
+        self.video_fps = int(cap.get(cv2.CAP_PROP_FPS))
         self.total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         if build_cache:
             self.build_cache()
         super().__init__(self.labels_dir, transforms, transform, target_transform)
 
-
     def find_video(self):
-        mp4_files = glob(self.video_folder+"/*.mp4")
+        mp4_files = glob(self.video_folder + "/*.mp4")
         wmv_files = glob(self.video_folder + "/*.wmv")
         files = mp4_files + wmv_files
         if len(files) > 1:
@@ -53,27 +55,61 @@ class VideoDataset(YOLODataset):
     def build_cache(self):
         """ Extract frames from video"""
         cap = self.open_video()
-        skip = self.calculate_skip()
-        frames_extracted = 0
-        postfix = "jpg"
-        frame_list = range(0,self.total_frames,int(skip))
+        frame_list = self.get_frame_list()
         for frame_num in tqdm(frame_list, desc=f"Build cache for {self.name}"):
-            filename = self.labels_dir + f"frame_{frame_num:06d}.{postfix}"
-            if not os.path.isfile(filename):
-                cap.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
-                res, frame_bgr = cap.read()
-                if res:
-                    frame_rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
-                    img = Image.fromarray(frame_rgb)
-                    img.save(filename)
-                if not res or not img:
-                    raise Exception(f"Can't read frame {frame_num}")
-            frames_extracted += 1
+            self.extract_frame(frame_num, cap)
+        cap.release()
+        self.delete_unused_frames(frame_list)
+        self.image_paths = self.get_image_paths(self.root)
+
+    def get_frame_list(self):
+        skip = int(self.calculate_skip())
+        frame_list = list(range(skip, self.total_frames, skip))
+        if self.desired_frames:
+            assert len(frame_list) == self.desired_frames
+        return frame_list
+
+    def extract_frame(self, frame_num, cap):
+        filename = self.get_frame_path(frame_num)
+        if not os.path.isfile(filename):
+            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
+            res, frame_bgr = cap.read()
+            if res:
+                frame_rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
+                img = Image.fromarray(frame_rgb)
+                img.save(filename)
+            if not res or not img:
+                raise Exception(f"Can't read frame {frame_num}")
+        return filename
+
+    def get_frame_path(self, frame_num):
+        postfix = "jpg"
+        filename = self.labels_dir + f"frame_{frame_num:06d}.{postfix}"
+        return filename
+
+    def delete_unused_frames(self, frames_to_preserve):
+        for frame_num in range(self.total_frames):
+            if frame_num not in frames_to_preserve:
+                fn = self.get_frame_path(frame_num)
+                if os.path.isfile(fn):
+                    os.unlink(fn)
+                    print("deleted", fn)
 
     def calculate_skip(self):
-        if not self.desired_frames:
+        if self.fps is None and self.desired_frames is None:
+            # Use all frames
             return 1
-        skip = (self.total_frames -1) / self.desired_frames
+        assert self.fps is None or self.desired_frames is None
+
+        # By FPS
+        if self.fps:
+            if self.fps > self.video_fps:
+                warnings.warn(f"Impossible increase fps from {self.video_fps} to {self.fps}")
+            skip = self.video_fps / self.fps
+            return skip
+
+        # By desired_frames
+        skip = (self.total_frames - 1) / self.desired_frames
         return max(skip, 1)
 
     @property
@@ -81,9 +117,18 @@ class VideoDataset(YOLODataset):
         return self.settings['desired_frames']
 
     @desired_frames.setter
-    def desired_frames(self,value):
+    def desired_frames(self, value):
         self.settings['desired_frames'] = value
+        self.settings['fps'] = None  # fps and desired_frames are conflicting properties
 
+    @property
+    def fps(self):
+        return self.settings['fps']
+
+    @fps.setter
+    def fps(self, value):
+        self.settings['desired_frames'] = None  # fps and desired_frames are conflicting properties
+        self.settings['fps'] = value
 
     def getfilename(self, n):
         """ get file with annotations for image on n place """
@@ -98,10 +143,22 @@ class VideoDataset(YOLODataset):
                 data = json.load(fp)
                 # merge settings
                 self.settings = {**self.settings, **data}
+        self.setup_defaults()
+
+    def setup_defaults(self):
+        defaults = ['desired_frames', 'fps']
+        for d in defaults:
+            if d not in self.settings:
+                self.settings[d] = None
+        if (self.desired_frames is not None) and (self.fps is not None):
+            warnings.warn(f"FPS and desired_frames are conflicting properties so desired_frames will be ignored")
+            self.desired_frames = None
+
 
     def save_settings(self, fn):
         with open(fn, 'w') as outfile:
             json.dump(self.settings, outfile)
+
     @property
     def name(self):
         parts = self.video_folder.split(os.sep)
@@ -109,4 +166,3 @@ class VideoDataset(YOLODataset):
         name = parts[-1].strip()
         assert len(name) > 0
         return name
-
